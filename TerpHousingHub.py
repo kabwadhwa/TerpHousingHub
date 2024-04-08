e07b5ababf8eebbee7dca8b41865a7bb686f9698
luceroz <53053902+luceroz@users.noreply.github.com>
2024-04-08 14:57:13 -0400

    Create TerpHousingHub.py

---
+++ TerpHousingHub.py
@@ -0,0 +1,21 @@
+""" 
+This script allows for UMD students to browse through studnet housing 
+options and leave reviews for their current housing.
+"""
+class Hub:
+    """ A class that represents a collection of housing options
+
+    Attributes:
+        Housing (list of Properties): a list containing all the
+            properties currently in our database of hosing options.
+    """
+class Property:
+    """ A class that represents single property listing
+
+    Attributes:
+        address (String)
+        ratings (list of floats)
+        on-campus (bool): true if the listing in on campus, false if listing is off-campus
+        apply (string): url or email to apply for the residence
+        
+    """
