a9527cb5389087567eef3dcceda995883d08edf3
luceroz <53053902+luceroz@users.noreply.github.com>
2024-04-10 13:53:17 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,1 +1,8 @@
-import tkinter
+from tkinter import *
+import tkinter as tk
+root = tk.Tk()
+root.title('TerpHousingHub')
+root.geometry('350x200')
+button = tk.Button(root, text = 'Stop', width = 25, command = root.destroy)
+button.pack()
+root.mainloop()
