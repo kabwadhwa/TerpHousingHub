ab57598543ebb1241cf3b2c51413d98b3018a352
luceroz <53053902+luceroz@users.noreply.github.com>
2024-04-10 17:46:10 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,8 +1,25 @@
 from tkinter import *
 import tkinter as tk
+
+#window
 root = tk.Tk()
 root.title('TerpHousingHub')
-root.geometry('350x200')
-button = tk.Button(root, text = 'Stop', width = 25, command = root.destroy)
-button.pack()
+root.geometry('750x600')
+
+# title
+title_label = Label(root, text = "Terp Housing Hub", font = 'Calibri 30')
+title_label.pack()
+
+#input field
+input_frame = Frame(root)
+search_label = Label(input_frame, text = "Search for a specific address to find reviews from past tennants or post a review yourself", font = 'Calibri 15')
+search_entry = Entry(input_frame)
+search_button = Button(input_frame, text='Search')
+search_label.pack()
+search_entry.pack()
+search_button.pack()
+input_frame.pack()
+
+
+
 root.mainloop()
